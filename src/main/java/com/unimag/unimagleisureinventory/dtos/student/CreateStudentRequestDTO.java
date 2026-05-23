f015defc1e60fbb326c2b3ce7de4a0bd87eef05a
LeonelP7 <pereiraleonel287@gmail.com>
2026-05-23 12:27:50 -0500

    feat: implementing CORS configuration and institucional email validation #28

--- src/main/java/com/unimag/unimagleisureinventory/dtos/student/CreateStudentRequestDTO.java
+++ src/main/java/com/unimag/unimagleisureinventory/dtos/student/CreateStudentRequestDTO.java
@@ -9,6 +9,7 @@ public record CreateStudentRequestDTO(
         @NotNull(message = "Student id cant be null") Long studentId,
         @NotBlank(message = "First name is required") String firstName,
         @NotBlank(message = "Last name is required") String lastName,
+        // TODO: la extension del email podria pasarse a una variable de entorno
         @NotBlank(message = "Email is required") @Email(message = "Invalid email format") @Pattern(
                 regexp = "^[a-zA-Z0-9._%+-]+@unimagdalena\\.edu\\.co$",
                 message = "Must be an institutional email @unimagdalena.edu.co"
