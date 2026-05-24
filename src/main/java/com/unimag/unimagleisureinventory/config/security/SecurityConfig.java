1a853d7faf19566e99ac837082f3459aa905b441
LeonelP7 <pereiraleonel287@gmail.com>
2026-05-24 10:45:37 -0500

    "feat: implementing OpenApi setup"

--- src/main/java/com/unimag/unimagleisureinventory/config/security/SecurityConfig.java
+++ src/main/java/com/unimag/unimagleisureinventory/config/security/SecurityConfig.java
@@ -34,6 +34,11 @@ public class SecurityConfig {
                 .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers(
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/v3/api-docs/**"
+                        ).permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
