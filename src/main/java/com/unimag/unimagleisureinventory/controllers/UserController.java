0f4d6ff0788e08ea6f4585d7bdc6c989f57efe24
arturoandresv <arturovelasquezo0711@gmail.com>
2026-05-27 13:44:36 -0500

    fix: role for userById

--- src/main/java/com/unimag/unimagleisureinventory/controllers/UserController.java
+++ src/main/java/com/unimag/unimagleisureinventory/controllers/UserController.java
@@ -24,7 +24,7 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("/{id}")
-    @PreAuthorize("hasRole('ADMIN')")
+    @PreAuthorize("isAuthenticated()")
     public ResponseEntity<PersonResponseDTO> getById(@PathVariable UUID id) {
         return ResponseEntity.ok(userService.getById(id));
     }
